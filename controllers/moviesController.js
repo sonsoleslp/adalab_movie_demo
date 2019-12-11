075ef0d54aef2471fa8529531fbdc004b6a6292f
sonsoleslp <sonsoleslp@hotmail.com>
2019-12-12 00:17:39 +0100

    Add genre filter

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,10 @@
-var movies = require("../models/index");
+var { movies } = require("../models");
 
 exports.index = (req, res, next) => {
-	res.send(movies);
+	const { genre } = req.query;
+	let results = movies.filter(movie => {
+		return genre ? movie.genres.indexOf(genre) !== -1 : true;
+	});
+
+	res.send(results);
 }
